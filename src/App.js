a714dacb83b2ba2120c5df2a8e0adef21088a3c0
randomlolstuff <puru0596@gmail.com>
2019-07-12 16:01:35 +0530

    trying navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component} from "react";
 import "./App.css";
+ import MineComponent from './myOwnComponent';
+import Users from "./users";
+import Contact from "./contact";
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Navbar from "./navbar"
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
+      <div>
+        <div><Navbar/></div>
         <h1> Hello, World! </h1>
+        <div><Users/></div>
+        <div><Contact/></div>
+      
+        <Router>
+          
+    <div>
+      <Route path="/" component={MineComponent} />
+      <Route path="/users" component={Users} />
+      <Route path="/contact" component={Contact} />
       </div>
+  </Router>
+  </div>
+
+        
+        
+      
+
+      
     );
   }
 }
---
+++ src/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+class Navbar extends React.Component{
+    render()
+    {
+        return(
+            
+  <Navbar expand="lg" variant="light" bg="light">
+    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+  </Navbar>
+
+
+
+  
+        )
+}
+}
+export default Navbar
