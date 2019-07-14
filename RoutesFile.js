f056180d6d47c5fb52e1f9afa1b215fb0b206158
randomlolstuff <puru0596@gmail.com>
2019-07-15 01:41:24 +0530

    added login page

--- RoutesFile.js
+++ RoutesFile.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, withRouter, Switch, Route, Redirect } from 'rea
 import Base from './src/Components/Layout/Base';
 import Contact from './src/Components/Pages/Contact'
 import Registration from './src/Components/Pages/Registration'
+import Login from './src/Components/Pages/Login'
 
 class RoutesFile extends React.Component {
     render() {
@@ -20,6 +21,8 @@ class RoutesFile extends React.Component {
                 />
                 <Route exact path='/Contact' component={Contact}/>
                 <Route exact path='/Registration' component={Registration}/>
+                <Route exact path='/Login' component={Login}/>
+
 
             </Router>
             
